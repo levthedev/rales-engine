963d4c10a1a350f77f7a67a6e0702e002c61ad2b
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-28 11:23:20 -0600

    delete old router PORO

--- app/models/PORO/router.rb
+++
@@ -1,7 +0,0 @@
-class Router < ActionDispatch::Routing::Mapper
-  def initialize(controller)
-    get "/#{controller}/find_all", to: "#{controller}#search_all"
-    get "/#{controller}/find", to: "#{controller}#search"
-    get "/#{controller}/random", to: "#{controller}#random"
-  end
-end
